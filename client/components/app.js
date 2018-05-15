a9ac9cbc580c04122ede47e65177df262dc2edab
francesco <fgiananti@gmail.com>
2018-05-15 07:44:18 +0200

    add bind to removeAll

--- client/components/app.js
+++ client/components/app.js
@@ -29,19 +29,30 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
   render () {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+  handleRemoveAll() {
+    alert('remove all');
+  }
   render () {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove all</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option} />)
         }
@@ -62,10 +73,21 @@ class Option extends React.Component {
 
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(option);
+    }
+  }
   render () {
     return (
       <div>
-        Options component here
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
