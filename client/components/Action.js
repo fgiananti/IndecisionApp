373ad8e79efcfee9837372455b54f6a9dcd4910a
francesco <fgiananti@gmail.com>
2018-05-16 13:59:24 +0200

    add react-modal

--- client/components/Action.js
+++ client/components/Action.js
@@ -5,7 +5,7 @@ const Action = (props) => {
     <div>
       <button
         onClick={props.handlePick}
-        disabled={props.hasOptions}
+        disabled={!props.hasOptions}
         >
         What should I do?
       </button>
